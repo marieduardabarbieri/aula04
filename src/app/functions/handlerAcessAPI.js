964a7efa20e72d29cff7c635219a28d231be04df
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2023-08-15 14:57:57 -0400

    lista criada

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,10 +1,18 @@
 'use server'
 
+const lista = [
+    {name: "Carol"},
+    {email: "Carol@gmail.com"},
+    {password: "Carol123"},
+    {token: "Carol"}
+];
+
 const getUserAuthenticated = (user) => {
+   
 
 }
 
 const getUsers = () =>{
-        
+    return lista;
 }
 export { getUsers, getUserAuthenticated };
