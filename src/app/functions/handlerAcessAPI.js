37339c34cdd5d8c97e2fe37e6ccac14363ca3bf3
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2023-09-07 18:02:56 -0400

    .

--- src/app/functions/handlerAcessAPI.js
+++ src/app/functions/handlerAcessAPI.js
@@ -1,25 +1,39 @@
 'use server'
 
 const lista = [
-    {name: "Carol",
-    email: "Carol@gmail.com",
-    password: "Carol123",
-    token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"}
+    {
+        name: "Carol de Andrade Lima",
+        email: "Carol@gmail.com",
+        password: "Carol123",
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+    {
+        name: "Ana Santos",
+        email: "Ana@gmail.com",
+        password: "Ana",
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    },
+    {
+        name: "Roberta Marques",
+        email: "Roberta@gmail.com",
+        password: "Roberta",
+        token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
+    }
 ];
 
 const getUserAuthenticated = (users) => {
-   {lista.map (user =>{
-    if(user.email === user.email && user.password === user.password){
-        return users;
+    let userAutent = {}
+    {
+        lista.map(user => {
+            if (users.email === user.email && users.password === user.password) {
+               userAutent = user;
+            }          
+        })
+        return userAutent;
     }
-    else{
-        return null;
-    }
-   })}
-
 }
 
-const getUsers = () =>{
+const getUsers = () => {
     return lista;
 }
 
