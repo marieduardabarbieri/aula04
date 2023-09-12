0b2939d31e6f4c44cf79e9d8ab1f15129e7c48a8
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2023-09-12 10:43:46 -0400

    salvo na localStorage

--- src/app/functions/handlerAcess.js
+++ src/app/functions/handlerAcess.js
@@ -10,6 +10,7 @@ const handlerAcessUser = async (user) => {
 
     if (isTokenValidate) {
         Cookies.set('token', userAuth.token, { expires: 1 });
+        localStorage.setItem("name", userAuth.name); //salvo na localStorage
     }
     return userAuth;
 }
