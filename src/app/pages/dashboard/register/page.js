fc2df5bb10b6706b75c5e376dde19c0d382d5bab
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2023-11-07 16:00:53 -0400

    handlerRegister atualizado pré pronto para futuro cadastro

--- src/app/pages/dashboard/register/page.js
+++ src/app/pages/dashboard/register/page.js
@@ -1,6 +1,11 @@
 'use client'
+
+import { postUser } from '@/app/functions/handlerAcessAPI';
+import { useRouter } from 'next/navigation';
+import React, { useState } from "react";
 import { ToastContainer, toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';  //importação da estilização da caixinha de notificação
+import { resolve } from 'styled-jsx/css';
 
 export const metadata = {  //title
     title: 'Register user',
@@ -8,11 +13,22 @@ export const metadata = {  //title
 }
 
 export default function DashboardRegister() {
+    const[user, setUser] = useState({
+        name: '',
+        email: '',
+        password: ''
+    });
+    const { push } = useRouter();
 
-    const handlerRegister =  (e) => {  //aviso para envio com sucesso de formulario
+    const handlerFormRegister = async (event) => {  //aviso para envio com sucesso de formulario
         e.preventDefault(); 
         try {
-            toast.success("Usuário registrado com sucesso!!!"); 
+            await postUser(user);
+            await new Promise((resolve) => {
+                toast.success("Usuário registrado com sucesso!!!"); 
+                setTimeout(resolve, 5000);
+            })
+            return push("/pages/dashboard");
         } catch {
           toast.error("Erro no registro do usuário. Tente novamente!"); //não tem como cair no erro, pois não tem condição
         }
@@ -44,7 +60,7 @@ export default function DashboardRegister() {
                             placeholder='Senha'
                             type='password'>
                         </input></div>
-                    <button className="enviar" onClick={handlerRegister} >Registrar </button>
+                    <button className="enviar" onClick={handlerFormRegister} >Registrar </button>
                 </form>
                 <ToastContainer/>
             </div>
