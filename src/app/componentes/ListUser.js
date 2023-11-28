c968bd066a580d6d0765a2b66f2b187541bfe91a
marieduardabarbieri <maria.barbieri@estudante.ifms.edu.br>
2023-11-28 15:11:27 -0400

    fazendo a pagian dinamica(clica no usuario e ele vai pra alteração)

--- src/app/componentes/ListUser.js
+++ src/app/componentes/ListUser.js
@@ -1,14 +1,17 @@
+import Link from "next/link";
 
-export default async function ListUser({lista}){
+export default async function ListUser({ lista }) {
     await new Promise((resolve) => setTimeout(resolve, 3000)); //tempo com o qual os usuários aparecerão na tela
-    return(
+    return (
         <div>
-             {lista.map((user, index) => { //map da lista dos nomes dos usuários autenticados
-                return(
-                <div className="usuarioEsp" key={index}>
-                     <p> {user.name}</p>
-                </div>
-                ) })}
+            {lista.map((user, index) => { //map da lista dos nomes dos usuários autenticados
+                return (
+                    <div className="usuarioEsp" key={index}>
+                        <Link href={`/pages/dashboard/alter/${user.id}`}>
+                            <p> {user.name}</p>
+                        </Link>
+                    </div>
+                )})}
         </div>
     )
 }
